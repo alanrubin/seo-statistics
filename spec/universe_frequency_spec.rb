ed3005f7c86248867d831fb07d51b5b5a1d9a48c
Alan Rubin <alanrubin@Administrators-MacBook-Pro.local>
2011-03-01 15:11:48 +0200

    Universe frequency test with fixture

--- spec/universe_frequency_spec.rb
+++ spec/universe_frequency_spec.rb
@@ -1,9 +1,16 @@
-$LOAD_PATH << File.join(File.expand_path(File.dirname(__FILE__) , '..', 'lib')
+#$LOAD_PATH << File.join(File.expand_path(File.dirname(__FILE__) , '..', 'lib')
 
 require 'universe_frequency'
 
 describe UniverseFrequency do
   context "when extracting universe frequency" do
+    before(:each) do
+      module Kernel
+        def open(*args)
+          File.new('./spec/fixtures/lazy_fox.html')
+        end
+      end
+    end
     it "should extract it as a number" do
       UniverseFrequency.new.google('lazy fox').should eq(7_980_000)
     end
