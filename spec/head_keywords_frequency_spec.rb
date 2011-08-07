b5a8445f86fcb7ebcbd9d78305a7a2b9f746d368
Alan Rubin <alan@frubin.com>
2011-08-07 22:23:50 +0300

    Adding metrics plugin describe for rspec

--- spec/head_keywords_frequency_spec.rb
+++ spec/head_keywords_frequency_spec.rb
@@ -1,17 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe HeadKeywordsFrequency do
+describe "MetricsPlugins" do
+  describe HeadKeywordsFrequency do
   
-  before(:each) do
-    @page = SeoStatistics::Page.new(File.new(File.dirname(__FILE__) + '/fixtures/first.html'))
-  end
+    before(:each) do
+      @page = SeoStatistics::Page.new(File.new(File.dirname(__FILE__) + '/fixtures/first.html'))
+    end
   
-  it "should have empty frequency if search words are not found" do
-    HeadKeywordsFrequency.new.run(@page, []).should have(0).entries
-  end
+    it "should have empty frequency if search words are not found" do
+      HeadKeywordsFrequency.new.run(@page, []).should have(0).entries
+    end
   
-  it "should have correct frequency for selected search words" do
-    HeadKeywordsFrequency.new.run(@page, ['my', 'keywords']).should include('my' => 2, 'keywords' => 2)
-    HeadKeywordsFrequency.new.run(@page, ['my']).should_not include('keywords')
+    it "should have correct frequency for selected search words" do
+      HeadKeywordsFrequency.new.run(@page, ['my', 'keywords']).should include('my' => 2, 'keywords' => 2)
+      HeadKeywordsFrequency.new.run(@page, ['my']).should_not include('keywords')
+    end
   end
 end
